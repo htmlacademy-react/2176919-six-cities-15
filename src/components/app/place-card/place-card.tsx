085a9a73bf0f6be50201a6f58baa3bcf845ffcf3
Floias <irina.butcher@gmail.com>
2024-02-22 13:19:40 +0500

    добавляет информацию в карточку

--- src/components/app/place-card/place-card.tsx
+++ src/components/app/place-card/place-card.tsx
@@ -1,21 +1,27 @@
 import { Offer } from '../../../mocks/data';
 
+function Premium(): JSX.Element {
+  return (
+    <div className="place-card__mark">
+      <span>Premium</span>
+    </div>
+  );
+}
+
 function PlaceCard(offer: Offer): JSX.Element {
-  const {title} = offer;
+  const {title, isPremium, price, type, previewImage, rating} = offer;
   return (
     <article className="cities__card place-card">
-      <div className="place-card__mark">
-        <span>Premium</span>
-      </div>
+      {isPremium ? <Premium /> : ''}
       <div className="cities__image-wrapper place-card__image-wrapper">
-        <a href="#">
-          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image"/>
+        <a href={previewImage}>
+          <img className="place-card__image" src={previewImage} width="260" height="200" alt="Place image"/>
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;120</b>
+            <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
           <button className="place-card__bookmark-button button" type="button">
@@ -27,14 +33,14 @@ function PlaceCard(offer: Offer): JSX.Element {
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{ width: '80%' }}></span>
+            <span style={{ width: `${rating * 20}%` }}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
           <a href="#">{title}</a>
         </h2>
-        <p className="place-card__type">Apartment</p>
+        <p className="place-card__type">{type.charAt(0).toUpperCase() + type.slice(1)}</p>
       </div>
     </article>
   );
