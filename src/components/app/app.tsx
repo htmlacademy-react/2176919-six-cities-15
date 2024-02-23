fda44df4765ee6d11411b4590ad8c80f47b3eba6
Floias <irina.butcher@gmail.com>
2024-02-23 16:42:16 +0500

    добавляет Layout

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -7,6 +7,7 @@ import Login from '../../pages/login/login';
 import NotFound from '../../pages/not-found/not-found';
 import Offer from '../../pages/offer/offer';
 import PrivateRoute from '../private-route/private-route';
+import Layout from '../layout/layout';
 
 type AppProps = {
   offersCount: number;
@@ -19,30 +20,35 @@ function App({ offersCount }: AppProps): JSX.Element {
         <Routes>
           <Route
             path={AppRoute.Root}
-            element={<Main offersCount={offersCount} />}
-          />
-          <Route
-            path={AppRoute.Login}
-            element={<Login />}
-          />
-          <Route
-            path={AppRoute.Favorites}
-            element={
-              <PrivateRoute
-                authorizationStatus={AuthorizationStatus.NoAuth}
-              >
-                <Favorites />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path={AppRoute.Offer}
-            element={<Offer />}
-          />
-          <Route
-            path="*"
-            element={<NotFound />}
-          />
+            element={<Layout />}
+          >
+            <Route
+              index
+              element={<Main offersCount={offersCount} />}
+            />
+            <Route
+              path={AppRoute.Login}
+              element={<Login />}
+            />
+            <Route
+              path={AppRoute.Favorites}
+              element={
+                <PrivateRoute
+                  authorizationStatus={AuthorizationStatus.NoAuth}
+                >
+                  <Favorites />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path={AppRoute.Offer}
+              element={<Offer />}
+            />
+            <Route
+              path="*"
+              element={<NotFound />}
+            />
+          </Route>
         </Routes>
       </BrowserRouter>
     </HelmetProvider>
