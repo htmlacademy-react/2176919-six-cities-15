3356f430a2b899cccc8a26fc71f3f1c4d92270d4
Floias <irina.butcher@gmail.com>
2024-02-23 15:33:08 +0500

    добавляет стили для 404 страницы

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -7,9 +7,9 @@ function NotFound (): JSX.Element {
         <title>6 cities. Not Found.</title>
       </Helmet>
       <div className="page__status-wrapper tabs__content">
-        <b className="page__status">404 Not Found</b>
-        <p className="page__status-description">The page you are looking for can not be found.</p>
-        <a className="page__status-link" href="#">Go to home page</a>
+        <b className="page__status" style={{ display: 'block', margin: '40px 5px', fontSize: '32px', textAlign: 'center' }}>404 Not Found</b>
+        <p className="page__status-description" style={{ display: 'block', fontSize: '20px', textAlign: 'center' }}>The page you are looking for can not be found.</p>
+        <a className="page__status-link" href="#" style={{ display: 'block', fontSize: '20px', textAlign: 'center', color: '#3366BB' }}>Go to home page</a>
       </div>
     </section>
   );
