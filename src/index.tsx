1b1d9647e0a3687fee9b3dff582fd286e1979c4b
Floias <irina.butcher@gmail.com>
2024-04-09 07:53:57 +0500

    меняет лимит показа уведомлений

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ToastContainer limit={2}/>
+      <ToastContainer limit={1}/>
       <App />
     </Provider>
   </React.StrictMode>
